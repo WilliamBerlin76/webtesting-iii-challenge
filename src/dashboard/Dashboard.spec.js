c5a73e2a6a5acf754c4bb7d800cc286e4f97e53a
WilliamBerlin76 <william.berlin76@gmail.com>
2019-10-14 17:44:39 -0500

    tests to check if correct text is displayed in display component

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -2,8 +2,23 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 
-import Dashboard from './Dashboard'
+import Dashboard from './Dashboard';
 
 test('dashboard renders correctly', () => {
   render(<Dashboard />)
+});
+
+test('buttons displaying', () => {
+    const toggleMock = jest.fn();
+    const { getByText } = render(
+        <Dashboard toggleClosed={toggleMock} toggleLocked={toggleMock} closed={false} locked={false} />
+    );
+
+    toggleMock();
+    getByText(/open/i)
+    getByText(/Unlocked/i)
+    getByText(/Lock Gate/i)
+    getByText(/Close Gat/i)
+
+    expect(toggleMock).toHaveBeenCalled();
 })
