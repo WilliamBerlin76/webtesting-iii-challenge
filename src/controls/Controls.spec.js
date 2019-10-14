fee33806498500fe3cfc66ae47f76d3c45c12ac7
WilliamBerlin76 <william.berlin76@gmail.com>
2019-10-14 15:54:57 -0500

    mock test for control component is passing but not sure if it is written well to test the app

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,9 +1,24 @@
 // Test away!
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Controls from './Controls';
 
 test('it renders correctly', () => {
-    render(<Controls/>)
+    expect(render(<Controls/>)).toMatchSnapshot()
 })
+
+test('lock/unlock button text changes when button is clicked', () => {
+    const setButtonTextMock = jest.fn();
+    const { getByText } = render(
+        <Controls toggleClosed={setButtonTextMock} closed={false}/>
+    );
+
+    fireEvent.click(getByText(/close gate/i));
+
+    expect(setButtonTextMock).toHaveBeenCalled();
+    expect(setButtonTextMock).toHaveBeenCalledTimes(1);
+    expect(fireEvent.click(getByText(/close gate/i)))
+});
+
+
