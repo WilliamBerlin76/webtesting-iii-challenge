c25b0b21847a0919b72cd26cef8f34ad1b93e190
WilliamBerlin76 <william.berlin76@gmail.com>
2019-10-14 21:17:58 -0500

    display component tests complete

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,6 +1,6 @@
 // Test away!
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, findByText } from '@testing-library/react';
 
 import Controls from './Controls';
 
@@ -8,7 +8,20 @@ test('controls render correctly', () => {
     expect(render(<Controls/>)).toMatchSnapshot()
 })
 
-test('lock/unlock button text changes when button is clicked', () => {
+test('open/close gate button text displays open gate when closed', () => {
+    const setButtonTextMock = jest.fn();
+    const { getByText } = render(
+        <Controls toggleClosed={setButtonTextMock} closed={true}/>
+    );
+
+    fireEvent.click(getByText(/open gate/i));
+
+    expect(setButtonTextMock).toHaveBeenCalled();
+    expect(setButtonTextMock).toHaveBeenCalledTimes(1);
+    expect(fireEvent.click(getByText(/open gate/i)))
+});
+
+test('open/close gate button text displays close gate when open', () => {
     const setButtonTextMock = jest.fn();
     const { getByText } = render(
         <Controls toggleClosed={setButtonTextMock} closed={false}/>
@@ -20,5 +33,6 @@ test('lock/unlock button text changes when button is clicked', () => {
     expect(setButtonTextMock).toHaveBeenCalledTimes(1);
     expect(fireEvent.click(getByText(/close gate/i)))
 });
+
 
 
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -2,8 +2,46 @@
 import React from 'react';
 
 import Display from './Display';
-import { render } from 'react-testing-library';
+import { render, fireEvent } from 'react-testing-library';
+import '@testing-library/jest-dom/extend-expect'
 
 test('Display is rendered', () => {
     expect(render(<Display />)).toMatchSnapshot()
+});
+
+test('displays closed if closed prop is true', () => {
+    const closedDisplayMock = jest.fn();
+    const { getByText } = render(
+        <Display closedClass={closedDisplayMock} closed={true} />
+    );
+    fireEvent.click(getByText(/closed/i))
+
+    
+});
+
+test('displays unlocked if locked prop is false', () => {
+    const lockedDisplayMock = jest.fn();
+    const { getByText } = render(
+        <Display openClass={lockedDisplayMock} locked={false} />
+    );
+    fireEvent.click(getByText(/unlocked/i))
 })
+
+test('lockedClass has green-led class when locked is false ', () => {
+    const { getByText } = render(
+        <Display />
+    )
+    const unlock = getByText(/unlocked/i)
+    expect(unlock).toBeInTheDocument()
+    expect(unlock).toHaveClass('green-led')
+});
+
+test('lockedClass class is red-led when locked is true', () => {
+    const { getByText } = render(
+        <Display locked={true}/>
+    )
+    const lock = getByText('Unlocked')
+    expect(lock).toBeInTheDocument()
+    expect(lock).toHaveClass('green-led')
+});
+
